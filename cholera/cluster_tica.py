ee74dbc73b6376a19783a1353fcb40e5cdc371e8
jchodera <john.chodera@choderalab.org>
2016-10-13 14:53:45 -0400

    Updated code to analyze all patients

--- cholera/cluster_tica.py
+++ cholera/cluster_tica.py
@@ -16,24 +16,27 @@ import seaborn
 import numpy as np
 import pandas as pd
 
-filename = 'A_gordon.sorted.txt'
+def read_patient_data(patient):
+    filename = '%s_gordon.taxa_count.sorted.txt' % patient
 
-# Load data
-df = pd.read_table(filename, delim_whitespace=True)
+    # Load data
+    df = pd.read_table(filename, delim_whitespace=True)
 
-# Extract sorted sample labels
-samples = list(df.index)
-nsamples = len(samples)
-print('samples = ')
-print(samples)
-print('nsamples = %d' % nsamples)
+    # Extract sorted sample labels
+    samples = list(df.index)
+    nsamples = len(samples)
+    print('samples = ')
+    print(samples)
+    print('nsamples = %d' % nsamples)
 
-# Extract feature names
-features = list(df.columns)
-nfeatures = len(features)
+    # Extract feature names
+    features = list(df.columns)
+    nfeatures = len(features)
 
-# Extract feature vectors
-patient_timeseries = df.as_matrix()
+    # Extract feature vectors
+    patient_timeseries = df.as_matrix()
+
+    return [samples, features, patient_timeseries]
 
 #
 # Analyze
@@ -53,18 +56,34 @@ lag_time = 1 # lag time (in sampling intervals)
 
 # Get colormap
 from matplotlib.pyplot import cm
-cmap = cm.rainbow(np.linspace(0,1,nsamples))
+
+# Read patient data
+patients = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+data = list()
+for (subplot_index, patient) in enumerate(patients):
+    # Load patient data
+    [samples, features, patient_timeseries] = read_patient_data(patient)
+    data.append(patient_timeseries)
+    nsamples = len(samples)
+    nfeatures = len(features)
+tica = tICA(n_components=n_components, lag_time=lag_time)
+projected_data = tica.fit_transform(data)
 
 # Make plots
 pdf_filename = 'cholera.pdf'
-patients = ['A']
+subplot_nx = 2
+subplot_ny = 4
 with PdfPages(pdf_filename) as pdf:
-    fig = plt.figure(figsize=(5.5,5.5))
-    for patient in patients:
-        subplot_index = 1
-        subplot_nx = 1
-        subplot_ny = 1
-        ax = plt.subplot(subplot_ny, subplot_nx, subplot_index)
+    fig = plt.figure(figsize=(5,10))
+    for (patient_index, patient) in enumerate(patients):
+        # Load patient data
+        [samples, features, patient_timeseries] = read_patient_data(patient)
+        nsamples = len(samples)
+        nfeatures = len(features)
+        projection = projected_data[patient_index]
+        cmap = cm.rainbow(np.linspace(0,1,nsamples))
+
+        ax = plt.subplot(subplot_ny, subplot_nx, patient_index+1)
         ax.set_color_cycle(cmap)
         try:
             # Try to compute tICA projection
@@ -78,6 +97,7 @@ with PdfPages(pdf_filename) as pdf:
             projection = projection[0] # select out only trajectory
 
             # Plot with time-varying color
+            ax.plot(projection[0,0], projection[0,1], 'v', markersize=5)
             for i in range(nsamples-1):
                 ax.plot(projection[i:i+2,0], projection[i:i+2,1], 'o-', markersize=3, linewidth=0.5)
 
@@ -89,12 +109,9 @@ with PdfPages(pdf_filename) as pdf:
             plt.xlabel('tIC 1', fontsize=7)
             plt.xticks([])
             plt.yticks([])
+            plt.axis([-3, +3, -3, +3])
 
-            if subplot_index % subplot_nx == 0:
-                ax2 = ax.twinx()
-                ax2.set_xticks([])
-                ax2.set_yticks([])
-
+            plt.axis('square')
 
         except Exception as e:
             print(e)
