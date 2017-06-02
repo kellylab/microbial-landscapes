654bcaa1df8b478840720e3b4c859ff2093c6067
wkc <williamkurtischang@gmail.com>
2017-06-02 17:52:44 -0400

    jsd bats by depth working

--- notebooks/hierarchical-states/utils.R
+++ notebooks/hierarchical-states/utils.R
@@ -264,3 +264,33 @@ MakeDistMatrix <- function(dt, id.x, id.y, value.cn = "distance") {
   rownames(dm) <- rn
   dm
 }
+
+#' Given list of things, return data.table of non-redundant pairs between
+#' things (including self-self).
+#'
+#' @param things vector of things
+#' @param prefix string, prefix of column names (e.g. "thing")
+#' @param suffix character length 2, suffix of column names (usually c("i", "j"), e.g.)
+#' @param sep separator joining prefix and suffix (e.g. ".")
+#'
+#' @return data.table with thing i in one column and thing j in another
+#' @export
+#'
+#' @examples
+MakeNonRedundantPairs <- function(things, prefix = "thing",
+                                  suffix = c("i", "j"), sep = ".") {
+  library(data.table)
+  N <- length(things)
+  x <- lapply(1:N, function(n) {
+    if (n == 1) {
+      j <- things
+    } else {
+      j <- things[-seq(n - 1)]
+    }
+    out <- data.table(i = things[n], j = j)
+    setnames(out, paste(prefix, suffix, sep = sep))
+    out
+  })
+  x <- rbindlist(x)
+  x
+}
