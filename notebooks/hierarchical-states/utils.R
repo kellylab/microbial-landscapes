7a60cc0c989ff2960feb38d423e3ee5f70314ce8
William K Chang <wkc@nobook-heir-2.local>
2017-06-04 23:49:22 -0400

    more bats by depth

--- notebooks/hierarchical-states/utils.R
+++ notebooks/hierarchical-states/utils.R
@@ -1,46 +1,3 @@
-NormDegrees <- function(matrix) {
-  # prevent bugging out on 1x1 mats
-  matrix <- as.matrix(matrix)
-  nhosts <- dim(matrix)[1]
-  nphages <- dim(matrix)[2]
-  host.degree.norm <- rowSums(matrix) / nphages
-  phage.degree.norm <- colSums(matrix) / nhosts
-  return(list('host'=host.degree.norm, 'phage'=phage.degree.norm))
-}
-
-Connectivity <- function(matrix) {
-  matrix <- as.matrix(matrix)
-  md <- dim(matrix)
-  if (md[1] == 0 | md[2] ==0) {
-    c <- 0
-  }
-  else {
-    c <- sum(matrix) / (md[1] * md[2])
-  }
-  return(c)
-}
-
-# get the size fraction (um) from which the host was collected by ID
-HostSizeFraction <- function(x) {
-  id <- unlist(strsplit(x[["member"]], "[.]"))[3]
-  if (id %in% c(54, 55, 56)) {
-    sf <- "63"
-  }
-  else if (id %in% c(51, 52, 53)) {
-    sf <- "5"
-  }
-  else if (id %in% c(48, 49, 50)) {
-    sf <- "1"
-  }
-  else if (id %in% c(45, 46, 47)) {
-    sf <- "0.2"
-  }
-  else {
-    sf <- NA
-  }
-  return(sf)
-}
-
 #' Generalized Jensen-Shannon divergence from matrix.
 #'
 #' @param x Matrix where each column is a discrete/binned probability distribution
@@ -70,74 +27,6 @@ genJSD <- function(x, w = NULL) {
   # the second term returns 1x1 matrix; as.numeric converts to scalar
   jsd <- entropy.plugin(wsum, unit = "log2") - as.numeric(Hi %*% w)
   return(jsd)
-}
-
-#' Function to output phage and protein eigenvector centralities.
-#' Protein centrality calculated from phage centrality.
-#' See Faust (1997), MEJ Newman (2004).
-#'
-#' @param u.scores dataframe/table with columns phage, host.prot, and u.phage
-#' @param norm normalize centrality scores? (currently unimplemented)
-#'
-#' @return list of 2 data frames: (phage centrality, protein centrality)
-#' @export
-#'
-#' @examples
-PPCentrality <- function(u.scores, by = "host.prot", norm=FALSE) {
-  # clear unnecessary columns and make a new data.table
-  u.work <- select(u.scores, phage, get(by), u.phage)
-  # rename target column, this allows using both clusters and KOs
-  setnames(u.work, by, "target")
-  u.mat <- dcast(u.work, phage ~ target, value.var = "u.phage")
-  phages <- u.mat$phage
-  u.mat[, phage := NULL]
-  u.mat <- as.matrix(u.mat)
-  rownames(u.mat) <- as.character(phages)
-  X <- tcrossprod(u.mat)
-  # eigenvals/vecs of the phage matrix
-  phage.eig <- eigen(X)
-  phage.centrality <- data.table(
-    phage = rownames(X),
-    centrality = abs(phage.eig$vectors[, 1])
-  )
-  prot.centrality <- t(u.mat) %*% phage.centrality$centrality
-  prot.centrality <- data.table(target = rownames(prot.centrality),
-                                centrality = prot.centrality[, 1])
-  # restore column name to what was input
-  setnames(prot.centrality, "target", by)
-  return(list(
-    phage = phage.centrality,
-    protein = prot.centrality))
-}
-
-#' Creates randomized infection submatrix by permuting columns and/or rows and
-#' calculates the generalized host and host protein JSDs
-PermRowColNullModel <- function(ph, im, hm, cols = TRUE, rows = FALSE) {
-  # get the infection submatrix for phages
-  sm <- im[, ph]
-  # permute rows and columns, preserving only total # infections
-  if (cols) {
-    # permute columns
-    # this changes 'which hosts are infected'
-    sm <- apply(sm, 2, function(v) {sample(v)})
-  }
-  if (rows) {
-    # DIVERGENCE CALCULATIONS FAIL IF ANY COLS ARE ALL 0
-    # so repeat until no cols sum to 0 (probably slow...sigh)
-    test <- rep(0, ncol(sm))
-    while (any(test == 0)) {
-      # permuting rows requires transpose...silly
-      # this changes 'which phages infect them'
-      sm <- t(apply(sm, 1, function(v) {sample(v)}))
-      test <- colSums(sm)
-    }
-  }
-  # calculate host jsd
-  host.jsd <- genJSD(sm)
-  # make the new hostprot-copies-in-host range matrix
-  ppmat <- hm %*% sm
-  hostprot.jsd <- genJSD(ppmat)
-  return(list(host.jsd, hostprot.jsd))
 }
 
 #' Given a circular ggraph layout, radially project the nodes outward.
@@ -226,12 +115,8 @@ create_layout_nmds <- function(graph, dm, ...) {
 #' @examples
 CoordCMDS <- function(dm, eig = TRUE, ...) {
   fit <- cmdscale(dist(dm), eig = eig, ...)
-  if (eig) {
-    xform <- fit$points
-    eigrank <- data.table(rank = seq(length(fit$eig)), value = fit$eig)
-  } else {
-    xform <- fit
-  }
+  xform <- fit$points
+  eigrank <- data.table(rank = seq(length(fit$eig)), value = fit$eig)
   colnames(xform) <- c("x", "y")
   rn <- rownames(xform)
   xform <- as.data.table(xform)
