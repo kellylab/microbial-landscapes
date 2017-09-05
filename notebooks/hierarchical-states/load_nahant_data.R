eb747bcffbaaa735f5a43f6e4c92471870c234fc
William K Chang <wkc@nobook-heir-2.local>
2017-09-05 18:14:31 -0400

    fixed nahant load script

--- notebooks/hierarchical-states/load_nahant_data.R
+++ notebooks/hierarchical-states/load_nahant_data.R
@@ -1,26 +1,32 @@
 library(tidyverse)
 library(data.table)
 
+scripts.dir <- "../../scripts/r/"
+source(paste0(scripts.dir, "lineage_2_taxonomy.R"))
+# make this a real package someday
 source("~/github_bitbucket/wkc.r-utils/df_2_mat.R")
-
 
 data.dir <- "../../data/"
 read_nahant <- function(fn, data.dir) {
-  # TODO complete this
   dat <- fread(paste0(data.dir, fn), header = TRUE)
-  dat <- melt(dat, id.vars = c("OTU", "ConsensusLineage"),
+  # lineage columns are named differently
+  if (fn == "BactNorm.txt") {
+    setnames(dat, "ConsensusLineage", "Lineage")
+  }
+  dat <- melt(dat, id.vars = c("OTU", "Lineage"),
                      variable.name = "day")
   dat[, day := as.numeric(as.character(day))]
   dat[, value := as.numeric(value)]
   # merge OTU counts by consensus lineage
-  dat[, .(freq = sum(value)), by = .(ConsensusLineage, day)]
+  dat[, .(freq = sum(value)), by = .(Lineage, day)]
   # parse lineage
-  # nothing is mapped to species resolution
-  dat[, ConsensusLineage := gsub(";", "", ConsensusLineage)]
-  dat[, c("foo", "kingdom", "phylum", "class", "order", "family",
-                 "genus") := tstrsplit(ConsensusLineage,
-                                       "[[:alpha:]]__")]
-  dat[, foo := NULL]
+  dat[, c("kingdom", 
+          "phylum", 
+          "class", 
+          "order", 
+          "family", 
+          "genus", 
+          "species") := lineage_2_taxonomy(Lineage), by = Lineage]
 }
 nahant <- lapply(list(pro = "BactNorm.txt", euk = "EukNorm.txt"), read_nahant,
                  data.dir = data.dir) %>%
