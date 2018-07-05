70b278bcc50611d6b70a7bf1b78b1e3085121dc0
wkc <williamkurtischang@gmail.com>
2018-07-05 16:03:57 -0400

    cholera analysis with rank mds 2d as filters

--- scripts/tda/cholera.R
+++ scripts/tda/cholera.R
@@ -3,16 +3,25 @@ library(TDAmapper)
 library(philentropy)
 library(cowplot)
 library(tidyr)
+library(igraph)
+library(ggraph)
+library(tidygraph)
 
 util.dir <- "../r/"
 
 source(paste0(util.dir, "load_cholera_data.R"))
+source("vertex.2.points.R")
+source("dist2knn.R")
+source("mapper.2.igraph.R")
+source("plot.mapper.R")
+source("sample.subgraphs.R")
 
 # load data ---------------------------------------------------------------
 
 gordon[, freq := count / sum(count), by = sample]
 gordon.samples <- unique(gordon[, .(sample, subject, diagnosis, id, hour)])
 gordon.samples[, idx := frank(hour), by = subject]
+gordon.samples[, progression := idx / max(idx), by = subject]
 distribs <- dcast(gordon, sample ~ otu, value.var = "freq", fill = 0)
 sample.names <- distribs$sample
 distribs <- as.matrix(distribs[, -1])
@@ -26,196 +35,199 @@ js.dist <- sqrt(as.matrix(js.dist[, -1]))
 rownames(js.dist) <- rn
 js.dist <- js.dist[sample.names, sample.names]
 
-# tdamapper method --------------------------------------------------------
+#' # K-nearest neighbor density
 
-mds2 <- cmdscale(js.dist, 2)
-po <- 70
-ni <- c(10, 10)
-mpr <- mapper2D(js.dist, filter_values = list(mds2[,1], mds2[,2]),
-                percent_overlap = po,
-                num_intervals = ni)
+# knn ---------------------------------------------------------------------
 
-# characterize mapper vertices
-vertices <- lapply(mpr$points_in_vertex, function(pts) {
-  data.table(sample = names(pts), sample.index = pts)
-})
-vertices <- rbindlist(vertices, idcol = "vertex")
-vertices[, name := paste0("v", vertex)] # for later record keeping
-source("k.first.R")
-k <- 10 # kNN density for each sample
-kNN <- apply(js.dist, 1, k.first, k = k)
-# plot density
-gordon.samples[, knn := kNN[sample]]
-v2s <- merge(vertices, gordon.samples, by = "sample")
-fstate <- function(x) {
-  sum(x == "diarrhea") / length(x)
-}
-vertices <- v2s[, .(fd = fstate(diagnosis),
-                    mean.t = mean(hour),
-                    mean.knn = mean(knn),
-                    size = .N),
-                    by = .(vertex, name)]
-library(igraph)
-library(ggraph)
-g1 <- graph.adjacency(mpr$adjacency, mode="undirected")
-setkey(vertices, vertex)
-for (att in c("name", "fd", "mean.t", "mean.knn", "size")) {
-  g1 <- set_vertex_attr(g1, att, V(g1), vertices[.(as.vector(V(g1)))][[(att)]])
-}
+
+k <- 10
+knn <- dist2knn(js.dist, k = k)
+gordon.samples[, knn := knn[sample]]
+ggplot(gordon.samples, aes(x = progression, y = knn)) +
+  geom_smooth() +
+  geom_point(aes(color = diagnosis)) +
+  facet_wrap(~ subject)
+
+#' Diarrhea samples only showing disease duration:
 setkey(gordon.samples, diagnosis)
 ggplot(gordon.samples["diarrhea"], aes(x = hour, y = knn)) +
+  geom_smooth() +
   geom_point() +
-  facet_wrap(~ subject, ncol = 2) +
-  labs(y = "kNN", x = "hour", title = "diarrhea samples")
+  facet_wrap(~ subject)
+
+# mds --------------------------------------------------------
+
+mds2 <- cmdscale(js.dist, 2, eig = TRUE)
+mds2$GOF # lossy
+
+#' MDS sketch suggests existence of at least 2 clusters.
+plot(mds2$points)
+
+#' MDS rank evens out density but preserves some of the patchiness
+rk.mds <- apply(mds2$points, 2, rank)
+plot(rk.mds)
+
+po <- 70
+ni <- c(10, 10)
+mpr <- mapper2D(js.dist, filter_values = list(rk.mds[,1], rk.mds[,2]),
+                percent_overlap = po,
+                num_intervals = ni)
+v2p <- vertex.2.points(mpr$points_in_vertex)
+v2p <- merge(v2p, gordon.samples, by.x = "point.name", by.y = "sample")
+vertices <- v2p[, .(mean.knn = mean(knn),
+                    f.state = sum(diagnosis == "diarrhea") / .N,
+                    mean.t = mean(hour),
+                    size = .N),
+                by = .(vertex, vertex.name)]
+setorder(vertices, vertex)
+graf <- mapper.2.igraph(mpr)
+graf <- graf %>%
+  as_tbl_graph %>%
+  activate(nodes) %>%
+  left_join(vertices, by = c("name" = "vertex.name"))
 
 # draw graphs -------------------------------------------------------------
 
 set.seed(1)
-kk.fr <- function(graf) {
-  l1 <- create_layout(graf, layout = "igraph", algorithm = "kk")
-  l1 <- create_layout(graf, layout = "igraph", algorithm = "fr",
-                      niter = 1000,
-                      coords = as.matrix(l1[, c("x", "y")]))
-  l1
-}
-lo <- kk.fr(g1)
-# lo <- create_layout(g1, layout = "igraph", algorithm = "fr", niter = 1000)
-V(g1)$x <- lo[V(g1), "x"] # save layout coordinates for later
-V(g1)$y <- lo[V(g1), "y"]
-source("plot.mapper.R")
+lo <- create_layout(graf, "fr", niter = 1000)
 # color by fraction samples marked diarrhea
-plot.mapper(lo, aes_(size = ~size, color = ~fd),
+plot.mapper(lo, aes_(size = ~size, color = ~f.state),
             list(size = "# samples", color = "f diarrhea")) +
   scale_color_distiller(palette = "Spectral")
 # color vertices by mean knn density
 plot.mapper(lo, aes_(size = ~size, color = ~mean.knn),
             list(size = "# samples", color = "mean kNN")) +
   scale_color_distiller(palette = "Spectral", direction = 1)
-# color by mean time in estimated hours
-plot.mapper(lo, aes_(size = ~size, color = ~mean.t), list(size = "samples")) +
-  scale_color_distiller(palette = "Spectral", direction = 1)
 
+
+#' # Subject trajectories
 # subject trajectories ----------------------------------------------------
-source("vertex.2.points.R")
-vertices <- vertex.2.points(mpr$points_in_vertex)
-setnames(vertices, "point.name", "sample")
-# produce a subgraph for every sample
-source("sample.subgraphs.R")
-sample.spx <- sample.subgraphs(
-  vertices[, .(samples = sample, vertices = vertex)], g1)
-sample.spx <- sample.spx[!is.na(sample.spx)]
-sample.overlays <- lapply(names(sample.spx), function(name, ss, lo) {
-  sg <- sample.spx[[name]]
-  if (grepl("diarrhea", name)) {
-    clr <- "red"
+graf <- graf %>%
+  activate(nodes) %>%
+  mutate(x = lo$x, y = lo$y)
+subject.phases <- v2p %>%
+  split(by = "subject") %>%
+  lapply(function(dt, graf) {
+    max.i <- max(dt$idx)
+    v <- dt[, .(progression = mean(progression)), by = .(vertex, vertex.name)]
+    sg <- graf %>%
+      slice(v$vertex)
+    sg %>%
+      activate(nodes) %>%
+      left_join(v, by = c("name" = "vertex.name"))
+
+  }, graf = graf)
+subject.phase.plots <- mapply(function(g, subj, global) {
+  d <- as.data.frame(g)
+  plot.mapper(global, aes_(size = ~size), labs(title = subj), color = "grey") +
+    geom_point(aes(x = x, y = y, size = size, color = progression), data = d) +
+    scale_color_distiller(palette = "Spectral", direction = 1)
+}, g = subject.phases, subj = names(subject.phases),
+MoreArgs = list(global = lo), SIMPLIFY = FALSE)
+for (i in seq_along(subject.phase.plots)) {
+  fn <- paste0("cholera-subject-trajectories/phases/",
+               names(subject.phase.plots)[i], ".png")
+  save_plot(fn, subject.phase.plots[[i]], base_height = 6)
+}
+
+#' ## "Top" arc progressions
+cl1 <- c("A", "F", "G")
+plot_grid(plotlist = subject.phase.plots[cl1], labels = cl1)
+
+#' ## Other
+cl.other <- c("B", "C", "D", "E")
+plot_grid(plotlist = subject.phase.plots[cl.other], labels = cl.other)
+
+# frames ------------------------------------------------------------------
+
+
+sample.sgs <- v2p %>%
+  split(by = "point.name") %>%
+  lapply(function(s, graf) {
+    slice(graf, s$vertex)
+  }, graf = graf)
+sample.frames <- mapply(function(sg, sample, state, graf) {
+  if (state == "diarrhea") {
+    color <- "red"
   } else {
-    clr <- "blue"
+    color <- "blue"
   }
-  plot.mapper(lo, aes_(size = ~size), list(title = name), color = "grey50") +
-    geom_point(aes(x = x, y = y, size = size),
-               data = as.data.frame(vertex_attr(sg)), color = clr)
-}, ss = sample.spx, lo = lo)
-names(sample.overlays) <- names(sample.spx)
-for (s in names(sample.overlays)) {
-  save_plot(paste0("subject-trajectories/frames/", s, ".png"),
-            sample.overlays[[s]])
+  d <- as.data.frame(sg)
+  plot.mapper(graf, aes_(size = ~size), labs(title = sample), color = "grey") +
+    geom_point(aes(x = x, y = y, size = size), data = d, color = color)
+}, sg = sample.sgs[gordon.samples$sample], sample = gordon.samples$sample,
+state = gordon.samples$diagnosis,
+MoreArgs = list(graf = lo), SIMPLIFY = FALSE)
+setorder(gordon.samples, subject, idx)
+for (i in seq(nrow(gordon.samples))) {
+  fn <- paste0("cholera-subject-trajectories/frames/",
+               gordon.samples[i, subject], gordon.samples[i, idx],
+               ".png")
+  save_plot(fn, sample.frames[[gordon.samples[i, sample]]])
 }
 
-# subject trace
-v2s[, c("x", "y") := lapply(c("x", "y"), function(att, graf, vs) {
-  a <- get.vertex.attribute(graf, att)
-  a[vs]
-}, graf = g1, vs = vertex)]
-sample.xy <- v2s[, lapply(list(x = x, y = y), mean),
-                 by = .(sample, sample.index, subject, diagnosis, id, hour,
-                        idx)]
-sample.xy <- split(sample.xy, by = "subject")
-straces <- mapply(function(dt, subj, lo) {
-  setorder(dt, idx)
-  n <- nrow(dt)
-  ends <- dt[c(1, n)]
-  dt <- data.frame(x = dt[-n, x], xend = dt[-1, x],
-                   y = dt[-n, y], yend = dt[-1, y],
-                   diagnosis = dt[-1, diagnosis])
-  ggraph(lo) +
-    geom_edge_link0(colour = "grey50") +
-    geom_node_point(aes(size = size), color = "grey50") +
-    theme(aspect.ratio = 1) +
-    theme_graph(base_family = "Helvetica") +
-    guides(size = FALSE) +
-    labs(title = subj) +
-    # geom_path(aes(x = x, y = y), dt) +
-    geom_point(aes(color = diagnosis, x = x, y = y), ends, size = 2) +
-    geom_segment(aes(x = x, y = y, xend = xend, yend = yend,
-                     color = diagnosis), dt,
-                 arrow = arrow(length = unit(5, "points"), type = "open"))
-}, dt = sample.xy, subj = names(sample.xy), MoreArgs = list(lo = lo),
-SIMPLIFY = FALSE)
-save_plot("subject-trajectories/all.pdf", plot_grid(plotlist = straces),
-          ncol = 3, base_height = 8, base_aspect_ratio = 0.5)
-
-#' # 'Meta' persistent homology on the Mapper graph
-#' ## b0 vs density threshold
-# b0 vs density threshold ----------------------------------------------
-
-ls <- sort(unique(V(g1)$mean.knn))
-ls.gs <- lapply(ls, function(x, g) induced_subgraph(g, V(g)$mean.knn <= x),
-                g = g1)
-comps <- lapply(ls.gs, components)
-# ncomps <- sapply(comps, function(g) sum(g$csize > 1)) # filter singletons
-ncomps <- sapply(comps, function(g) g$no)
-ggplot(data.frame(threshold = ls, b0 = ncomps), aes(x = threshold, y = b0)) +
-  geom_line() + geom_point()
-
-#' Get the longest interval in density threshold where b0 is constant.
-#' First get the number of intervals in the threshold for each run where b0 is
-#' constant:
-b0.runs <- rle(ncomps)
-b0.runs <- data.table(length = b0.runs$lengths, b0 = b0.runs$values)
-#' Cumulatively sum over intervals in the threshold value to get the end
-#' threshold value of each run:
-b0.runs[, end := sapply(cumsum(length), function(n, th) {
-  th[n]
-}, th = ls)]
-#' Shift the end values down and prepend 0 for the start threshold value of
-#' each run:
-b0.runs[, start := c(0, end[-.N])]
-#' Get persistence length of each run in the dimension of the threshold.
-b0.runs[, persistence := end - start]
-#' Get rid of the run between 0 and the first point.
-b0.runs <- b0.runs[-1,]
-#' Find the threshold at the end of the run with maximum persistence
-thc <- b0.runs[b0 > 1, end[persistence == max(persistence)]]
-
-#' Get the vertices in that level set:
-rep.vs <- which(V(g1)$mean.knn <= thc)
-rep.xy <- lo[rep.vs, c("x", "y")]
-rep.g <- induced_subgraph(g1, rep.vs)
-V(rep.g)$state <- as.character(comps[[which(ls == thc)]]$membership)
-rep.lo <- create_layout(rep.g, "manual", node.positions = rep.xy)
-plot.mapper(rep.lo, aes_(size = ~size, color = ~state), list(size = "samples"))
-
-#' ## Video of graph expansion as vertices are added from least to most dense
-# global density level set video frames -----------------------------------
-lvlset.grafs <- lapply(ls, function(th, graf, layout) {
-  vids <- which(V(graf)$mean.knn <= th)
-  sg <- induced_subgraph(graf, vids)
-  xl <- c(min(V(graf)$x) - 1, max(V(graf)$x) + 1)
-  yl <- c(min(V(graf)$y) - 1, max(V(graf)$y) + 1)
-  cl <- sapply(c("min", "max"), function(fn, x) {
-    do.call(fn, list(x= x))
-    }, x = V(graf)$mean.knn)
-  ggraph(sg, "manual", node.positions = data.frame(x = V(sg)$x, y = V(sg)$y)) +
-    geom_edge_link0() +
-    geom_node_point(aes(color = mean.knn, size = size)) +
-    coord_cartesian(xlim = xl, ylim = yl) +
-    scale_color_gradient2(midpoint = median(V(graf)$mean.knn),
-                          high = "blue", mid = "yellow", low = "red",
-                          limits = cl) +
-    theme_graph()
-}, graf = g1, layout = lo)
-for (i in seq_along(ls)) {
-  fn <- paste0("level-set/frames/", i, ".png")
-  save_plot(fn, lvlset.grafs[[i]])
-}
-
+#' #' # 'Meta' persistent homology on the Mapper graph
+#' #' ## b0 vs density threshold
+# # b0 vs density threshold ----------------------------------------------
+#
+# ls <- sort(unique(V(g1)$mean.knn))
+# ls.gs <- lapply(ls, function(x, g) induced_subgraph(g, V(g)$mean.knn <= x),
+#                 g = g1)
+# comps <- lapply(ls.gs, components)
+# # ncomps <- sapply(comps, function(g) sum(g$csize > 1)) # filter singletons
+# ncomps <- sapply(comps, function(g) g$no)
+# ggplot(data.frame(threshold = ls, b0 = ncomps), aes(x = threshold, y = b0)) +
+#   geom_line() + geom_point()
+#
+#' #' Get the longest interval in density threshold where b0 is constant.
+#' #' First get the number of intervals in the threshold for each run where b0 is
+#' #' constant:
+# b0.runs <- rle(ncomps)
+# b0.runs <- data.table(length = b0.runs$lengths, b0 = b0.runs$values)
+# #' Cumulatively sum over intervals in the threshold value to get the end
+# #' threshold value of each run:
+# b0.runs[, end := sapply(cumsum(length), function(n, th) {
+#   th[n]
+# }, th = ls)]
+# #' Shift the end values down and prepend 0 for the start threshold value of
+# #' each run:
+# b0.runs[, start := c(0, end[-.N])]
+# #' Get persistence length of each run in the dimension of the threshold.
+# b0.runs[, persistence := end - start]
+# #' Get rid of the run between 0 and the first point.
+# b0.runs <- b0.runs[-1,]
+# #' Find the threshold at the end of the run with maximum persistence
+# thc <- b0.runs[b0 > 1, end[persistence == max(persistence)]]
+#
+# #' Get the vertices in that level set:
+# rep.vs <- which(V(g1)$mean.knn <= thc)
+# rep.xy <- lo[rep.vs, c("x", "y")]
+# rep.g <- induced_subgraph(g1, rep.vs)
+# V(rep.g)$state <- as.character(comps[[which(ls == thc)]]$membership)
+# rep.lo <- create_layout(rep.g, "manual", node.positions = rep.xy)
+# plot.mapper(rep.lo, aes_(size = ~size, color = ~state), list(size = "samples"))
+#
+# #' ## Video of graph expansion as vertices are added from least to most dense
+# # global density level set video frames -----------------------------------
+# lvlset.grafs <- lapply(ls, function(th, graf, layout) {
+#   vids <- which(V(graf)$mean.knn <= th)
+#   sg <- induced_subgraph(graf, vids)
+#   xl <- c(min(V(graf)$x) - 1, max(V(graf)$x) + 1)
+#   yl <- c(min(V(graf)$y) - 1, max(V(graf)$y) + 1)
+#   cl <- sapply(c("min", "max"), function(fn, x) {
+#     do.call(fn, list(x= x))
+#     }, x = V(graf)$mean.knn)
+#   ggraph(sg, "manual", node.positions = data.frame(x = V(sg)$x, y = V(sg)$y)) +
+#     geom_edge_link0() +
+#     geom_node_point(aes(color = mean.knn, size = size)) +
+#     coord_cartesian(xlim = xl, ylim = yl) +
+#     scale_color_gradient2(midpoint = median(V(graf)$mean.knn),
+#                           high = "blue", mid = "yellow", low = "red",
+#                           limits = cl) +
+#     theme_graph()
+# }, graf = g1, layout = lo)
+# for (i in seq_along(ls)) {
+#   fn <- paste0("level-set/frames/", i, ".png")
+#   save_plot(fn, lvlset.grafs[[i]])
+# }
+#
--- scripts/tda/make-animations.sh
+++
@@ -1,6 +0,0 @@
-for x in A B C D E F G
-do
-		cat subject-trajectories/frames/$x*.png | ffmpeg -framerate 2 -f image2pipe -i - \
-				-pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" \
-				subject-trajectories/animations/$x.mp4
-done
