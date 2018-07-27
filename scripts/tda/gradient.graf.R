9932279e57398b2d19cee2d5bb8834be1f3620b2
wkc <williamkurtischang@gmail.com>
2018-07-27 13:59:57 -0400

    basin

--- scripts/tda/gradient.graf.R
+++ scripts/tda/gradient.graf.R
@@ -2,16 +2,30 @@ gradient.graf <- function(in_graf, values, down = TRUE) {
   if (class(values) == "character") {
     values <- vertex_attr(in_graf, values)
   }
-  ej.mat <- as.matrix(as.data.frame(activate(in_graf, edges)))
-  ej.mat <- rbind(ej.mat, ej.mat[, c(2, 1)])
-  keep <- apply(ej.mat, 1, function(vs, values, down) {
+  ej <- as.data.frame(activate(in_graf, edges))
+  from <- ej$from
+  to <- ej$to
+  delta <- values[to] - values[from]
+  rectify <- function(from, to, delta, down) {
+    newfrom <- from
+    newto <- to
     if (down) {
-      values[vs[1]] >= values[vs[2]]
+      if (delta > 0) {
+        newfrom <- to
+        newto <- from
+      }
     } else {
-      values[vs[1]] <= values[vs[2]]
+      if (delta < 0) {
+        newfrom <- to
+        newto <- from
+      }
     }
-  }, values = values, down = down)
+    c(from = newfrom, to = newto, weight = abs(delta))
+  }
+  new.ej <- mapply(rectify, from = from, to = to, delta = delta,
+                   MoreArgs = list(down = down))
+  new.ej <- as.data.frame(t(new.ej))
   tbl_graph(nodes = as.data.frame(activate(in_graf, nodes)),
-            edges = as.data.frame(ej.mat[keep, ]),
+            edges = new.ej,
             directed = TRUE)
 }
